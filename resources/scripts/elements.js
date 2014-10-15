1cbcfd4c1e199ba8c45bd090146a4b2493d7703b
Paul Dijou <paul.dijou@gmail.com>
2014-10-15 16:56:26 +1300

    Update website post conference

--- resources/scripts/elements.js
+++ resources/scripts/elements.js
@@ -4,6 +4,7 @@ module.exports = {
   banner: $(document.getElementById('banner')),
   about: $(document.getElementById('about')),
   register: $(document.getElementById('register')),
+  speakers: $(document.getElementById('speakers')),
   schedule: $(document.getElementById('schedule')),
   venue: $(document.getElementById('venue')),
   isSmall: function isSmall() {  return window.innerWidth < 992; }
--- resources/scripts/map.js
+++ resources/scripts/map.js
@@ -1,30 +1,30 @@
-var styles = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill"},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#7dcdcd"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]}];
-
-var centerPosition = new google.maps.LatLng(-36.853522, 174.760981);
-var venuePosition = new google.maps.LatLng(-36.862549,174.7603857);
-var hotelPosition = new google.maps.LatLng(-36.846738, 174.761957);
-
-var options = {
-  zoom: 14,
-  center: centerPosition,
-  mapTypeId: google.maps.MapTypeId.ROADMAP,
-  scaleControl: false,
-	scrollwheel: false,
-  styles: styles
-};
-
-var map = new google.maps.Map(document.getElementById('map'), options);
-
-new google.maps.Marker({
-  position: venuePosition,
-  map: map,
-  title: 'Movio',
-  icon: 'resources/images/marker-venue.png'
-});
-
-new google.maps.Marker({
-  position: hotelPosition,
-  map: map,
-  title: 'Heritage Hotel',
-  icon: 'resources/images/marker-hotel.png'
-});
+// var styles = [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill"},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"water","stylers":[{"color":"#7dcdcd"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]}];
+//
+// var centerPosition = new google.maps.LatLng(-36.853522, 174.760981);
+// var venuePosition = new google.maps.LatLng(-36.862549,174.7603857);
+// var hotelPosition = new google.maps.LatLng(-36.846738, 174.761957);
+//
+// var options = {
+//   zoom: 14,
+//   center: centerPosition,
+//   mapTypeId: google.maps.MapTypeId.ROADMAP,
+//   scaleControl: false,
+// 	scrollwheel: false,
+//   styles: styles
+// };
+//
+// var map = new google.maps.Map(document.getElementById('map'), options);
+//
+// new google.maps.Marker({
+//   position: venuePosition,
+//   map: map,
+//   title: 'Movio',
+//   icon: 'resources/images/marker-venue.png'
+// });
+//
+// new google.maps.Marker({
+//   position: hotelPosition,
+//   map: map,
+//   title: 'Heritage Hotel',
+//   icon: 'resources/images/marker-hotel.png'
+// });
