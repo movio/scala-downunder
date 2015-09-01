9431c6098f78f05896bfce125d620af7c9ed9dde
Nicolas Maquet <nicolas@movio.co>
2015-09-02 10:34:04 +1200

    update deploy script

--- _gulp/deploy.js
+++ _gulp/deploy.js
@@ -7,13 +7,13 @@ gulp.task('deploy', function() {
   global.isWatching = false;
   return gulp.src('', {read: false})
     .pipe($.shell([
-      'rm _site/resources/app.min*',
+      'rm -f _site/resources/app.min*',
       'gulp build --no-watch',
       'jekyll build --future',
       'gulp usemin',
       'gulp minifyHtml',
-      'rm _site/resources/app.css',
-      'rm _site/resources/app.js',
+      'rm -f _site/resources/app.css',
+      'rm -f _site/resources/app.js',
       'cd _site && git add . -A',
       'cd _site && git commit -m "Publish website"',
       'cd _site && git push -f origin gh-pages'
